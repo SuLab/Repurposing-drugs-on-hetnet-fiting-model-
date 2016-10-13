1058a3a056dc55e4cf055ce63351cc899bf6f147
NuriaQueralt <nqueralt.r@gmail.com>
2016-10-13 14:08:00 -0700

    Added experiment-2 called '2-hetionet-reduced', ie C-D-G. Added integration and graph building, ie added 'integrate' dir and subdirs with necessary input files and scripts.

---
+++ 2-hetionet-reduced/integrate/build.sh
@@ -0,0 +1,15 @@
+set -o errexit
+
+# Create the network
+jupyter nbconvert --execute integrate.ipynb --inplace --ExecutePreprocessor.timeout=-1
+
+# Create permuted networks
+jupyter nbconvert --execute permute.ipynb --inplace --ExecutePreprocessor.timeout=-1
+
+# Evaluate permution effectiveness
+jupyter nbconvert --execute data/permuted/evaluate-permutations.ipynb --inplace --FilesWriter.build_directory=data/permuted --ExecutePreprocessor.timeout=-1
+
+# Export to neo4j
+jupyter nbconvert --execute neo4j-import.ipynb --inplace --ExecutePreprocessor.timeout=-1
+#./usr/bin/python3 neo4j-import.py
+
---
+++ 2-hetionet-reduced/integrate/precompile.sh
@@ -0,0 +1,4 @@
+#jupyter nbconvert --execute compile/AeG-expression.ipynb --inplace --FilesWriter.build_directory=compile --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute compile/DaG-association.ipynb --inplace --FilesWriter.build_directory=compile --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute compile/CbG-binding.ipynb --inplace --FilesWriter.build_directory=compile --ExecutePreprocessor.timeout=-1
+
